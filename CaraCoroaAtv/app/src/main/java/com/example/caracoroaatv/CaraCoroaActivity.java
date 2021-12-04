a6c3f45b40c210072ba7ff222ae7e1ee6b56152d
Wheados <Wheadosz@hotmail.com>
2021-12-04 08:34:32 -0300

    caracoroa java code partial

---
+++ CaraCoroaAtv/app/src/main/java/com/example/caracoroaatv/CaraCoroaActivity.java
@@ -0,0 +1,27 @@
+package com.example.caracoroaatv;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ImageView;
+
+import java.util.Random;
+
+public class CaraCoroaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cara_coroa);
+
+        Random r = new Random();
+        int valor = r.nextInt() % 2;
+        ImageView caraCoroaImagem = findViewById(R.id.caraCoroaImg);
+
+        if (valor == 0){
+            caraCoroaImagem.setBackgroundResource(R.drawable.moeda_cara);
+        } else{
+            caraCoroaImagem.setBackgroundResource(R.drawable.moeda_coroa);
+        }
+    }
+}
